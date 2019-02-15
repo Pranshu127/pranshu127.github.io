5dd6bee0f60fbf03da593be925c4c117e6f52e3f
Pranshu127 <47073662+Pranshu127@users.noreply.github.com>
2019-02-15 23:44:03 +0530

    First push , puvblishing the app on github

---
+++ src/Expt.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { MDBContainer, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBMDBNavLink, MDBIcon,MDBNavLink } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Carousel from './Carousel';
+
+class FixedNavbarExample extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          collapse: false,
+      };
+      this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+        collapse: !this.state.collapse,
+      });
+  }
+
+  render() {
+    const bgPink = {backgroundColor: '#e91e63'}
+    const container = {height: 1300}
+    return(
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar style={bgPink} dark expand="md" scrolling fixed="top">
+              <MDBNavbarBrand href="/">
+                  <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={ this.onClick } />
+              <MDBCollapse isOpen = { this.state.collapse } navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                      <MDBNavLink to="#">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                      <MDBNavLink to="#">Features</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                      <MDBNavLink to="#">Pricing</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Options</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="instagram" /></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
+        <MDBContainer style={container} className="text-center mt-5 pt-5">
+         <Carousel/>
+        </MDBContainer>
+      </div>
+    );
+  }
+}
+
+export default FixedNavbarExample;
---
+++ src/Head.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Head extends React.Component
+{
+    render(){
+    return(
+    <h1>My name is anthony Gonsalves!!</h1>
+     );
+}
+}
+export default Head;
