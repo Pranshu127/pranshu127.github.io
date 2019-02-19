5e1398fafccfe0f1a7a6c56aebb31640aa577d72
Pranshu127 <47073662+Pranshu127@users.noreply.github.com>
2019-02-19 16:15:29 +0530

    This is a branch that fixes the card issue;

--- src/Expt.js
+++ src/Expt.js
@@ -1,71 +1,42 @@
 import React from 'react';
-import { MDBContainer, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBMDBNavLink, MDBIcon,MDBNavLink } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Carousel from './Carousel';
 
-class FixedNavbarExample extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          collapse: false,
-      };
-      this.onClick = this.onClick.bind(this);
-  }
-
-  onClick() {
-    this.setState({
-        collapse: !this.state.collapse,
-      });
-  }
-
-  render() {
-    const bgPink = {backgroundColor: '#e91e63'}
-    const container = {height: 1300}
+class Expt extends React.Component{
+render(){
     return(
-      <div>
-        <Router>
-          <header>
-            <MDBNavbar style={bgPink} dark expand="md" scrolling fixed="top">
-              <MDBNavbarBrand href="/">
-                  <strong>Navbar</strong>
-              </MDBNavbarBrand>
-              <MDBNavbarToggler onClick={ this.onClick } />
-              <MDBCollapse isOpen = { this.state.collapse } navbar>
-                <MDBNavbarNav left>
-                  <MDBNavItem active>
-                      <MDBNavLink to="#">Home</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                      <MDBNavLink to="#">Features</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                      <MDBNavLink to="#">Pricing</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#">Options</MDBNavLink>
-                  </MDBNavItem>
-                </MDBNavbarNav>
-                <MDBNavbarNav right>
-                  <MDBNavItem>
-                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#"><MDBIcon fab icon="instagram" /></MDBNavLink>
-                  </MDBNavItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
-            </MDBNavbar>
-          </header>
-        </Router>
-        <MDBContainer style={container} className="text-center mt-5 pt-5">
-         <Carousel/>
-        </MDBContainer>
-      </div>
-    );
-  }
-}
+        <div>
+            <div class="container">
+              <div class="row">
+                <div class="col-4">.col-4</div>
+                <div class="col-4">.col-4</div>
+                <div class="col-4">.col-4</div>
+              </div>
 
-export default FixedNavbarExample;
+              <div class="row">
+                <div class="col-sm-4">.col-sm-4</div>
+                <div class="col-sm-4">.col-sm-4</div>
+                <div class="col-sm-4">.col-sm-4</div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">.col-md-4</div>
+                <div class="col-md-4">.col-md-4</div>
+                <div class="col-md-4">.col-md-4</div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-4">.col-lg-4</div>
+                <div class="col-lg-4">.col-lg-4</div>
+                <div class="col-lg-4">.col-lg-4</div>
+              </div>
+
+              <div class="row">
+                <div class="col-xl-4">.col-xl-4</div>
+                <div class="col-xl-4">.col-xl-4</div>
+                <div class="col-xl-4">.col-xl-4</div>
+              </div>
+            </div>
+        </div>
+    )
+}
+}
+export default Expt;
